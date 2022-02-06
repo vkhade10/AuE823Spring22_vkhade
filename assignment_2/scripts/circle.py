398a9fb45ee3b931bb0e69f4f420216bdbc7dfa6
vkhade10 <vkhade@g.clemson.edu>
2022-02-05 19:44:05 -0500

    Lab 2 and Assignment 2

---
+++ assignment_2/scripts/circle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+
+def turtle_circle(r):
+	rospy.init_node('turtlesim', anonymous=True)
+	pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+	rate = rospy.Rate(10)
+	vel = Twist()
+	while not rospy.is_shutdown():
+		vel.linear.x = r
+		vel.linear.y = 0
+		vel.linear.z = 0
+		vel.angular.x = 0
+		vel.angular.y = 0
+		vel.angular.z = 1
+		rospy.loginfo("Radius = %f",
+					r)
+		pub.publish(vel)
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	try:
+		r = input("r = ")
+		turtle_circle(float(r))
+	except rospy.ROSInterruptException:
+		pass
